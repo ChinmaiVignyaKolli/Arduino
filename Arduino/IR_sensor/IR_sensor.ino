efaa4c0ca9cf265564686cfa77a142c84c35ae13
Chinmai Vignya Kolli <51255436+ChinmaiVignyaKolli@users.noreply.github.com>
2022-08-23 02:52:25 +0530

    Add files via upload

---
+++ Arduino/IR_sensor/IR_sensor.ino
@@ -0,0 +1,23 @@
+int LED = 13;
+int sensorPin=7;
+
+void setup() {
+ pinMode(LED,OUTPUT);
+ pinMode(sensorPin,INPUT);
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int value=digitalRead(sensorPin);
+if(value==1)
+{
+  digitalWrite(LED,HIGH);
+}
+else
+{
+  digitalWrite(LED,LOW);
+}
+delay(200);
+}
